172c3fdc242bf5df9b1ae0c52332ae3fa80c9273
keyWasFull <sleepio1@hotmail.com>
2022-12-09 00:11:46 -0600

    more diffs

---
+++ bringInNewStuff.bat
@@ -0,0 +1,2 @@
+del "C:\Users\sleep\Documents\DevTools\Hearthstone\Hearthstone_Data\Managed\Assembly-CSharp.dll"
+copy "C:\Users\sleep\Documents\DevTools\Decompiled\Assembly-CSharp\obj\Release\net472\Assembly-CSharp.dll" "C:\Users\sleep\Documents\DevTools\Hearthstone\Hearthstone_Data\Managed\Assembly-CSharp.dll"
